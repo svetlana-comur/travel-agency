59eb33ef10b9a62bb3e7112b1719fc840a4eb524
svetlana-comur <svetlana.comur@isa.utm.md>
2026-04-21 00:49:57 +0300

    Implemented DataAccess layer(UserContext, DbSession)

--- TravelAgency.DataAccess/Context/UserContext.cs
+++ TravelAgency.DataAccess/Context/UserContext.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TravelAgency.Domains.Entities.User;
+using Microsoft.EntityFrameworkCore;
 
 namespace TravelAgency.DataAccess.Context
 {
-    internal class UserContext
+    public class UserContext : DbContext
     {
+
+        public DbSet<UserData> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DbSession.ConnectionStrings);
+        }
     }
 }
